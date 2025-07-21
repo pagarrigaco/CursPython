1c95a1480e7965c96ff9f02803aa8f7abb9156ad
Pau <paugarrigacomas@gmail.com>
2025-07-22 00:27:34 +0200

    Dia2b

---
+++ Dia 2/Conversions.py
@@ -0,0 +1,24 @@
+num1 = 20 #integer
+num2 = 30.5 #float
+
+num1 = num1+num2 #conversio implicita
+
+print(type(num1))
+print(type(num2))
+
+num3 = 5.8
+print(num3)
+print(type(num3))
+
+num4 = int(num3) #conversió explicita
+
+print(num4)
+print(type(num4))
+
+edat = input("Escriu la teva edat: ")
+
+print(type(edat))
+
+edat = int(edat)
+
+print(type (edat))
