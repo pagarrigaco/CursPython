b3293b5737a45649fd4b25264d37a908194cad96
PaGarrigaCo <paugarrigacomas@gmail.com>
2025-12-16 15:26:02 +0100

    Variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+message = "Hello World!"
+print(message)
+
+nom = 'pau'
+print(nom.title())
+print(nom.upper())
+print(nom.lower())
+cognom = "garriga"
+nom_complet =f"{nom} {cognom}"
+print(f"Hola {nom_complet.title()}, benvingut!")
+
